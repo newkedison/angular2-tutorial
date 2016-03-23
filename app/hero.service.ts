ae9202f4240721a2eba3255d524ea6d74876f919
newkedison <newkedison@gmail.com>
2016-03-23 15:46:12 +0800

    Tour of Heroes: Part 5
    See: https://angular.io/docs/ts/latest/tutorial/toh-pt5.html
    **The Road Behind**
    
    We travelled a great distance in this chapter.
    
    * We added the Angular Component Router to navigate among different components.
    * We learned how to create router links to represent navigation menu items
    * We used router parameters to navigate to the details of user selected hero
    * We shared the HeroService among multiple components
    * We moved HTML and CSS out of the component file and into their own files.
    * We added the uppercase pipe to format data

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -1,9 +1,16 @@
-import {Injectable} from 'angular2/core';
-import {Hero} from './hero';
-import {HEROES} from './mock-heroes';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import { Injectable } from 'angular2/core';
+
 @Injectable()
 export class HeroService {
   getHeroes() {
     return Promise.resolve(HEROES);
+  }
+
+  getHero(id: number) {
+    return Promise.resolve(HEROES).then(
+      heroes => heroes.filter(hero => hero.id === id)[0]
+    );
   }
 }
